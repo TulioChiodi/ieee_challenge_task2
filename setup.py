c65f2be07b319261612be2155c3f58f4036f5da3
tuliochiodi <tuliochiodi@gmail.com>
2021-05-15 14:23:34 -0300

    setup.py updated

--- setup.py
+++ setup.py
@@ -4,4 +4,17 @@ setup(
     name='src',
     version='0.1.0',
     packages=find_packages(),   
+    install_requires=[
+        'jiwer==2.2.0',
+        'librosa==0.8.0',
+        'numpy==1.18.1',
+        'pandas==1.0.3',
+        'pystoi==0.3.3',
+        'scipy==1.4.1',
+        'soundfile==0.10.3.post1',
+        'torch==1.0.1',
+        'transformers==4.4.2',
+        'tqdm==4.36.1',
+        'wget==3.2',
+    ]
 )   
